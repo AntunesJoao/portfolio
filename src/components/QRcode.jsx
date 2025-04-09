4255ad7d48dd64bb07013b60e2626c9b7c3762bb
AntunesJoão <antunesjoao81@gmail.com>
2025-04-09 00:45:18 -0400

    atualizando QRcode

---
+++ src/components/QRcode.jsx
@@ -0,0 +1,55 @@
+import './QRcode.css'
+import { BsXLg } from "react-icons/bs";
+
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+
+
+import QRPage from "../img/QRPage.png";
+function QRcode ({setShow}) {
+  return (
+      <div className="fundo">
+                  <div className="card-fundo">
+                    <div className="nav-btn">
+                    <button onClick={() => setShow(false)} className="btn-card">
+                    <BsXLg />
+                      </button>
+                    </div>
+                    <div className="fundo-card-control">
+                      <img src={QRPage} alt="foto card" />
+                      <div className="descrica"><h3>Tecnologias utilizadas:</h3>
+                                                <ul>
+                                                  <li>HTML</li>
+                                                  <li>Css</li>
+                                                  <li>JavaScript</li>
+                                                  <li>API</li>
+                                                  </ul>
+                                                  
+                                                  <p>Basta escrever algo e o codigo irá gerar um QRcode vinculado
+                                                    ao que foi escrito. Assim que apontar a camera, levará direto a ele.
+                                                    Usei uma API para o QRcode.</p></div>
+                      <div className="code">
+                        <SyntaxHighlighter language='javascript' style={dracula}>
+                            {`function generateQrCode(){
+                                const qrCodeInputValue = qrCodeInput.value
+
+                                if(!qrCodeInputValue)return
+                                qrCodebtn.innerText = "Gerando codigo..."
+    
+                                qrCodeImg.src =  " https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrCodeInputValue
+
+                                qrCodeImg.addEventListener("load", ()=>{  
+                                container.classList.add("active")
+                                 qrCodebtn.innerText = "Cdigo criado."
+    })
+}`}
+                        </SyntaxHighlighter>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+  )
+}
+
+export default QRcode
--- src/routes/Port.jsx
+++ src/routes/Port.jsx
@@ -1,29 +1,41 @@
 import "./Port.css";
+import { useState } from "react";
 
-import QRPage from "../img/QRPage.png";
+
+
+
+import QRHome from "../img/QRHome.png";
 import BlogHome from "../img/BlogHome.png";
 import Calculadorahome from "../img/Calculadorahome.png";
 import GLPI from "../img/GLPI.png";
 import Cobrinha from "../img/Cobrinha.png";
-import Hdhost from '../img/Hdhost.png'
+import Hdhost from "../img/Hdhost.png";
 
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+import QRcode from "../components/QRcode";
 
 const Port = () => {
+  const [show, setShow] = useState(false);
+
   return (
     <div className="port-control">
       <div className="port-colum">
         <div className="card-control">
           <h2>Gerador de QRcode</h2>
-          <img src={QRPage} alt="Foto do projeto" />
-          <button className="btn">Ler mais</button>
+          <img src={QRHome} alt="Foto do projeto" />
+          <button className="btn" onClick={() => setShow(!show)}>
+            Ver descriçao
+          </button>
+
+          {show && <QRcode setShow ={setShow}/>}
         </div>
+
         <div className="card-control">
           <h2>Blog com Axios</h2>
           <img src={BlogHome} alt="Foto do projeto" />
           <button className="btn">Ler mais</button>
         </div>
+
         <div className="card-control">
           <h2>Calculadora de IMC</h2>
           <img src={Calculadorahome} alt="Foto do projeto" />
