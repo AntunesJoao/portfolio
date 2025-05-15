25dc4584750d0b28a5f84ce660c21dc14e81c3de
AntunesJoão <antunesjoao81@gmail.com>
2025-05-15 13:17:47 -0400

    adicionando animacao na home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -7,16 +7,9 @@ const Home = () => {
     <div className="main">
       <div className="info">
         <p>Óla, eu sou</p>
-        <h1>João Antunes</h1>
+        <h1 className="typewriter">João Antunes</h1>
         <p>Desenvolvedor de software apaixonador
           por criar sites <br></br> e experiências interativas na web </p>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>React</li>
-          <li>Angular</li>
-        </ul>
       </div>
       <div className="img-control">
         <img src={MinhaFoto} alt="Minha foto" />
