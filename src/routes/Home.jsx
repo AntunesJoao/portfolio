8d75f214b63f7c6a1f6296269c400de79149834a
AntunesJoão <antunesjoao81@gmail.com>
2025-05-15 15:52:42 -0400

    adicionando botao de download CV

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,7 +2,20 @@ import React from "react";
 import MinhaFoto from "../img/foto.png";
 import "./Home.css";
 
+//icones
+import { HiOutlineDocumentDownload } from "react-icons/hi";
+
 const Home = () => {
+
+  const handleDownload = () => {
+  const link = document.createElement('a');
+  link.href = '/Antunes.pdf';
+  link.download = 'Antunes.pdf';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
+
   return (
     <div className="main">
       <div className="info">
@@ -10,6 +23,9 @@ const Home = () => {
         <h1 className="typewriter">João Antunes</h1>
         <p>Desenvolvedor de software apaixonador
           por criar sites <br></br> e experiências interativas na web </p>
+        
+            <button className="btn" onClick={handleDownload}>Baixar Curriculo <HiOutlineDocumentDownload /></button>
+         
       </div>
       <div className="img-control">
         <img src={MinhaFoto} alt="Minha foto" />
